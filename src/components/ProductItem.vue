3007873c60f280095740a34754d23cac5b4d2251
JasGH <yasaman.gonehi@gmail.com>
2021-07-18 16:09:00 +0430

    emit added

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -3,17 +3,16 @@
     class="product-item-box"
     elevation="0"
   >
-    <v-img
-      max-width="200"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-      class="product-item-box-image"
-    />
-    <v-card-text>
-      آیتم محصول
-    </v-card-text>
-    <v-card-text>
-      {{ product.title }}
-    </v-card-text>
+    <div @click="showInfo">
+      <v-img
+        max-width="200"
+        :src="product.photo"
+        class="product-item-box-image"
+      />
+      <v-card-text class="product-item-box-title">
+        {{ product.title }}
+      </v-card-text>
+    </div>
     <v-card-actions>
       <v-btn
         color="deep-purple lighten-2"
@@ -23,6 +22,18 @@
         <i class="fi-rr-shopping-cart-add" />
       </v-btn>
     </v-card-actions>
+    <div class="product-item-box-prices">
+      <v-chip
+        color="#ef5350"
+        class="product-item-box-prices-sheet"
+      >
+        30%
+      </v-chip>
+      <v-card-text>
+        {{ product.price.discount }}
+        {{ product.price.final }}
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 <script>
@@ -40,8 +51,14 @@ export default {
   },
   methods:{
     addToCart(){
-      this.$emit('chosenProduct', this.product)
+      this.$emit('addToCart', this.product)
+    },
+    showInfo(){
+      this.$emit('showInfo', this.product)
     }
+  },
+  created() {
+    console.log(this.product)
   }
 }
 </script>
@@ -54,5 +71,40 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+.product-item-box-image{
+  border-radius: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.v-card__subtitle, .v-card__text, .v-card__title {
+  padding: 0 0 0;
+}
+.product-item-box-title{
+  padding-right: 10px;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: right;
+  color: #ffffff;
+}
+.product-item-box-prices{
+  display: flex;
+  align-items: center;
+}
+.product-item-box-prices-sheet{
+  width: 60px;
+  height: 44px;
+  position: relative;
+  left: 87px;
+  top: 1px;
+}
+.v-chip.v-size--default {
+  border-radius: 25px 0;
+  font-size: 14px;
+  height: 44px;
 }
 </style>

9c43085d85e07a56d9c7de8f413f2be1ff113a07
mitraKhm <mitra.kh1995@gmail.com>
2021-07-18 05:54:03 -0700

    cart item - base style

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -7,18 +7,75 @@
     <p class="title">
       سبد خرید
     </p>
-    <cart-item
-      v-for="item in 5"
-      :key="item"
-    />
+    <div class="cart-item-box">
+      <cart-item
+        v-for="item in 5"
+        :key="item"
+        class="cart-item mx-7"
+      />
+    </div>
+    <v-card
+      color="#3a3b55"
+      class="order-box"
+    >
+      <div class="mr-9 mb-7 pa-3">
+        <div class="d-flex mb-7">
+          <span class="ml-8">
+            مبلغ خام : 4,500,000
+          </span>
+          <span>
+            میزان تخفیف : 4,500,000
+          </span>
+        </div>
+        <div>
+          <span class="ml-7">
+            مبلغ نهایی و قابل پرداخت:
+          </span>
+          <span>
+            3,150,000 تومان
+          </span>
+        </div>
+      </div>
+
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-btn
+            color="#4caf50"
+            height="60"
+            width="100%"
+            class="submit-btn"
+          >
+            ادامه و ثبت سفارش
+          </v-btn>
+        </v-col>
+        <v-col cols="3 text-center">
+          <v-btn
+            color="#484967"
+            height="60"
+            class="delete-btn"
+          >
+            <v-icon>
+              mdi-delete-outline
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-card>
 </template>
 
 <script>
 import CartItem from './CartItem';
+import {Cart} from '../Models/Cart';
 export default {
   name: 'Cart',
-  components: {CartItem}
+  components: {CartItem},
+  props:{
+    value:{
+      type:Cart,
+      default: new Cart()
+    }
+  }
 }
 </script>
 
@@ -30,12 +87,29 @@ export default {
   border-radius: 20px;
   position: -webkit-sticky;
   position: sticky;
-  top: 75px;
+  top:10px;
   max-height: calc( 100vh - 86px );
-  height: calc( 100vh - 86px );
   .title{
+    font-size: 20px;
     padding-top: 20px;
     margin-right: 30px;
+    margin-bottom: 20px;
   }
+  .cart-item-box{
+    max-height: 600px;
+    overflow: auto;
+  }
+  .order-box{
+    border-radius: 20px;
+    width: 100%;
+    .submit-btn{
+      border-radius: 15px;
+      margin-right: 15px;
+    }
+    .delete-btn{
+      border-radius: 15px;
+    }
+  }
+
 }
 </style>

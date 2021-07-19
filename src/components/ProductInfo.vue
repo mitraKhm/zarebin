f57e0aaa07baa7b3ef2ac386e4234880e8477a3c
neokazemi <neo.kazemi168@gmail.com>
2021-07-19 16:33:42 +0430

    toast

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -70,6 +70,7 @@
             <v-btn
               height="56px"
               color="#f7bb41"
+              @click="addToCart"
             >
               افزودن به سبد
               <i class="fi-rr-shopping-cart-add relative-top" />
